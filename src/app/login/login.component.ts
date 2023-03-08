27381be746715f93ea0b056bf731dfd346a8c540
pascuord <pascuord@gmail.com>
2023-03-08 23:03:13 +0100

    Set title tag to component page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private title:Title) { 
+  }
+
+  ngOnInit(): void {
+    this.title.setTitle("Login");
+  }
+
+}
